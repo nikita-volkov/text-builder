079c40db353220aa0cee3e85a10e1e4b8e23d959
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-16 16:53:44 +0300

    Reimplement with newtype

--- library/Text/Builder.hs
+++ library/Text/Builder.hs
@@ -75,173 +75,175 @@ import qualified TextBuilderDev as Dev
 -- |
 -- Specification of how to efficiently construct strict 'Text'.
 -- Provides instances of 'Semigroup' and 'Monoid', which have complexity of /O(1)/.
-type Builder = Dev.TextBuilder
+newtype Builder
+  = Builder Dev.TextBuilder
+  deriving (Show, IsString, Semigroup, Monoid)
 
 -- | Get the amount of characters
 {-# INLINE length #-}
 length :: Builder -> Int
-length = Dev.length
+length = coerce Dev.length
 
 -- | Check whether the builder is empty
 {-# INLINE null #-}
 null :: Builder -> Bool
-null = Dev.null
+null = coerce Dev.null
 
 -- | Execute a builder producing a strict text
 run :: Builder -> Text
-run = Dev.buildText
+run = coerce Dev.buildText
 
 -- ** Output IO
 
 -- | Put builder, to stdout
 putToStdOut :: Builder -> IO ()
-putToStdOut = Dev.putToStdOut
+putToStdOut = coerce Dev.putToStdOut
 
 -- | Put builder, to stderr
 putToStdErr :: Builder -> IO ()
-putToStdErr = Dev.putToStdErr
+putToStdErr = coerce Dev.putToStdErr
 
 -- | Put builder, followed by a line, to stdout
 putLnToStdOut :: Builder -> IO ()
-putLnToStdOut = Dev.putLnToStdOut
+putLnToStdOut = coerce Dev.putLnToStdOut
 
 -- | Put builder, followed by a line, to stderr
 putLnToStdErr :: Builder -> IO ()
-putLnToStdErr = Dev.putLnToStdErr
+putLnToStdErr = coerce Dev.putLnToStdErr
 
 -- * Constructors
 
 -- | Unicode character
 {-# INLINE char #-}
 char :: Char -> Builder
-char = Dev.char
+char = coerce Dev.char
 
 -- | Unicode code point
 {-# INLINE unicodeCodePoint #-}
 unicodeCodePoint :: Int -> Builder
-unicodeCodePoint = Dev.unicodeCodePoint
+unicodeCodePoint = coerce Dev.unicodeCodePoint
 
 -- | Single code-unit UTF-16 character
 {-# INLINEABLE utf16CodeUnits1 #-}
 utf16CodeUnits1 :: Word16 -> Builder
-utf16CodeUnits1 = Dev.utf16CodeUnits1
+utf16CodeUnits1 = coerce Dev.utf16CodeUnits1
 
 -- | Double code-unit UTF-16 character
 {-# INLINEABLE utf16CodeUnits2 #-}
 utf16CodeUnits2 :: Word16 -> Word16 -> Builder
-utf16CodeUnits2 = Dev.utf16CodeUnits2
+utf16CodeUnits2 = coerce Dev.utf16CodeUnits2
 
 -- | Single code-unit UTF-8 character
 {-# INLINE utf8CodeUnits1 #-}
 utf8CodeUnits1 :: Word8 -> Builder
-utf8CodeUnits1 = Dev.utf8CodeUnits1
+utf8CodeUnits1 = coerce Dev.utf8CodeUnits1
 
 -- | Double code-unit UTF-8 character
 {-# INLINE utf8CodeUnits2 #-}
 utf8CodeUnits2 :: Word8 -> Word8 -> Builder
-utf8CodeUnits2 = Dev.utf8CodeUnits2
+utf8CodeUnits2 = coerce Dev.utf8CodeUnits2
 
 -- | Triple code-unit UTF-8 character
 {-# INLINE utf8CodeUnits3 #-}
 utf8CodeUnits3 :: Word8 -> Word8 -> Word8 -> Builder
-utf8CodeUnits3 = Dev.utf8CodeUnits3
+utf8CodeUnits3 = coerce Dev.utf8CodeUnits3
 
 -- | UTF-8 character out of 4 code units
 {-# INLINE utf8CodeUnits4 #-}
 utf8CodeUnits4 :: Word8 -> Word8 -> Word8 -> Word8 -> Builder
-utf8CodeUnits4 = Dev.utf8CodeUnits4
+utf8CodeUnits4 = coerce Dev.utf8CodeUnits4
 
 -- | ASCII byte string
 {-# INLINEABLE asciiByteString #-}
 asciiByteString :: ByteString -> Builder
-asciiByteString = Dev.asciiByteString
+asciiByteString = coerce Dev.asciiByteString
 
 -- | Strict text
 {-# INLINEABLE text #-}
 text :: Text -> Builder
-text = Dev.text
+text = coerce Dev.text
 
 -- | String
 {-# INLINE string #-}
 string :: String -> Builder
-string = Dev.string
+string = coerce Dev.string
 
 -- | Decimal representation of an integral value
 {-# INLINEABLE decimal #-}
 decimal :: Integral a => a -> Builder
-decimal = Dev.decimal
+decimal = coerce . Dev.decimal
 
 -- | Decimal representation of an unsigned integral value
 {-# INLINEABLE unsignedDecimal #-}
 unsignedDecimal :: Integral a => a -> Builder
-unsignedDecimal = Dev.unsignedDecimal
+unsignedDecimal = coerce . Dev.unsignedDecimal
 
 -- | Decimal representation of an integral value with thousands separated by the specified character
 {-# INLINEABLE thousandSeparatedDecimal #-}
 thousandSeparatedDecimal :: Integral a => Char -> a -> Builder
-thousandSeparatedDecimal = Dev.thousandSeparatedDecimal
+thousandSeparatedDecimal = fmap coerce . Dev.thousandSeparatedDecimal
 
 -- | Decimal representation of an unsigned integral value with thousands separated by the specified character
 {-# INLINEABLE thousandSeparatedUnsignedDecimal #-}
 thousandSeparatedUnsignedDecimal :: Integral a => Char -> a -> Builder
-thousandSeparatedUnsignedDecimal = Dev.thousandSeparatedUnsignedDecimal
+thousandSeparatedUnsignedDecimal = fmap coerce . Dev.thousandSeparatedUnsignedDecimal
 
 -- | Data size in decimal notation over amount of bytes.
 {-# INLINEABLE dataSizeInBytesInDecimal #-}
 dataSizeInBytesInDecimal :: Integral a => Char -> a -> Builder
-dataSizeInBytesInDecimal = Dev.dataSizeInBytesInDecimal
+dataSizeInBytesInDecimal = fmap coerce . Dev.dataSizeInBytesInDecimal
 
 -- | Unsigned binary number
 {-# INLINE unsignedBinary #-}
 unsignedBinary :: Integral a => a -> Builder
-unsignedBinary = Dev.unsignedBinary
+unsignedBinary = coerce . Dev.unsignedBinary
 
 -- | Unsigned binary number
 {-# INLINE unsignedPaddedBinary #-}
 unsignedPaddedBinary :: (Integral a, FiniteBits a) => a -> Builder
-unsignedPaddedBinary = Dev.unsignedPaddedBinary
+unsignedPaddedBinary = coerce . Dev.unsignedPaddedBinary
 
 -- | Hexadecimal representation of an integral value
 {-# INLINE hexadecimal #-}
 hexadecimal :: Integral a => a -> Builder
-hexadecimal = Dev.hexadecimal
+hexadecimal = coerce . Dev.hexadecimal
 
 -- | Unsigned hexadecimal representation of an integral value
 {-# INLINE unsignedHexadecimal #-}
 unsignedHexadecimal :: Integral a => a -> Builder
-unsignedHexadecimal = Dev.unsignedHexadecimal
+unsignedHexadecimal = coerce . Dev.unsignedHexadecimal
 
 -- | Decimal digit
 {-# INLINE decimalDigit #-}
 decimalDigit :: Integral a => a -> Builder
-decimalDigit = Dev.decimalDigit
+decimalDigit = coerce . Dev.decimalDigit
 
 -- | Hexadecimal digit
 {-# INLINE hexadecimalDigit #-}
 hexadecimalDigit :: Integral a => a -> Builder
-hexadecimalDigit = Dev.hexadecimalDigit
+hexadecimalDigit = coerce . Dev.hexadecimalDigit
 
 -- | Intercalate builders
 {-# INLINE intercalate #-}
 intercalate :: Foldable foldable => Builder -> foldable Builder -> Builder
-intercalate = Dev.intercalate
+intercalate a b = coerce (Dev.intercalate (coerce a) (foldr ((:) . coerce) [] b))
 
 -- | Pad a builder from the left side to the specified length with the specified character
 {-# INLINEABLE padFromLeft #-}
 padFromLeft :: Int -> Char -> Builder -> Builder
-padFromLeft = Dev.padFromLeft
+padFromLeft = coerce Dev.padFromLeft
 
 -- | Pad a builder from the right side to the specified length with the specified character
 {-# INLINEABLE padFromRight #-}
 padFromRight :: Int -> Char -> Builder -> Builder
-padFromRight = Dev.padFromRight
+padFromRight = coerce Dev.padFromRight
 
 -- |
 -- Time interval in seconds.
 -- Directly applicable to 'DiffTime' and 'NominalDiffTime'.
 {-# INLINEABLE intervalInSeconds #-}
 intervalInSeconds :: RealFrac seconds => seconds -> Builder
-intervalInSeconds = Dev.intervalInSeconds
+intervalInSeconds = coerce . Dev.intervalInSeconds
 
 -- | Double with a fixed number of decimal places.
 {-# INLINE fixedDouble #-}
@@ -250,7 +252,7 @@ fixedDouble ::
   Int ->
   Double ->
   Builder
-fixedDouble = Dev.fixedDouble
+fixedDouble = coerce Dev.fixedDouble
 
 -- | Double multiplied by 100 with a fixed number of decimal places applied and followed by a percent-sign.
 {-# INLINE doublePercent #-}
@@ -259,9 +261,9 @@ doublePercent ::
   Int ->
   Double ->
   Builder
-doublePercent = Dev.doublePercent
+doublePercent = coerce Dev.doublePercent
 
 -- | Hexadecimal readable representation of binary data.
 {-# INLINE hexData #-}
 hexData :: ByteString -> Builder
-hexData = Dev.hexData
+hexData = coerce Dev.hexData
